0e1f4acb39226ced6864fbb7331590887989a650
Vanderson <vanderson@mercurio>
2022-12-19 09:00:45 -0300

    update send_config.py

--- send_config.py
+++ send_config.py
@@ -30,32 +30,24 @@ with open("devices_dellos") as f_dellos10:
 # NX-OS module 
 def dev_conn_nxos(ips, model):
     for ips in devices_nxos:
-        try:
-            # call net_conn module there are all connection model (nxos, ios, dellos) 
-            iosv = net_conn.netmiko_ios(ips)
-            print(f"\n{'#'*79}\nConnecting to device model: {model.upper()}\nHostname: {ips}\n")
-            net_connect = ConnectHandler(**iosv)
-            command1 = net_connect.send_config_from_file(f'rbac_{model.lower()}.cfg')
-            print(f"\n--------------- APPLIED CONFIG ---------------\n\n")
-            print(command1)
-
-            print("\n\n--------------- POST CONFIG ---------------\n")
-            command2 = net_connect.send_command("show run | in username")
-            print(command2)
-            end_time = datetime.now()
-    
-        except:
-            print(f"get an Error in device {ips}")
-        break
-    
-    print("Total time: {}".format(end_time - start_time))
+        # call net_conn module there are all connection model (nxos, ios, dellos) 
+        iosv = net_conn.netmiko_ios(ips)
+        print(f"\n{'#'*79}\nConnecting to device model: {model.upper()}\nHostname: {ips}\n")
+        net_connect = ConnectHandler(**iosv)
+        command1 = net_connect.send_config_from_file(f'rbac_{model.lower()}.cfg')
+        print(f"\n--------------- APPLIED CONFIG ---------------\n\n")
+        print(command1)
+        print("\n\n--------------- POST CONFIG ---------------\n")
+        command2 = net_connect.send_command("show run | in username")
+        print(command2)
+        end_time = datetime.now()
+        print("Total time: {}".format(end_time - start_time))
 
 dev_conn_nxos(devices_nxos, "nxos")
 
 # Dellos module
 def dev_conn_dellos10(ips, model):
     for ips in devices_dell:
-        print()
         iosv = net_conn.netmiko_dellos10(ips)
         print(f"\n{'#'*79}\nConnecting to device model: {model.upper()}\nHostname: {ips}\n")
         net_connect = ConnectHandler(**iosv)
@@ -74,7 +66,6 @@ dev_conn_dellos10(devices_dell, "dellos10")
 # IOS module
 def dev_conn_ios(ips, model):
     for ips in devices_ios:
-        print()
         iosv = net_conn.netmiko_ios(ips)
         print(f"\n{'#'*79}\nConnecting to device model: {model.upper()}\nHostname: {ips}\n")
         net_connect = ConnectHandler(**iosv, default_enter="\r\n")
